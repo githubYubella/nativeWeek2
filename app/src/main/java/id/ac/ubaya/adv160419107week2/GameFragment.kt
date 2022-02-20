2ac2f8d8326b55aaf1ec7e57b921e5374180c4a2
githubYubella <anmpbella@gmail.com>
2022-02-20 14:50:27 +0700

    finish tugas week 2

--- app/src/main/java/id/ac/ubaya/adv160419107week2/GameFragment.kt
+++ app/src/main/java/id/ac/ubaya/adv160419107week2/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class GameFragment : Fragment() {
 
@@ -21,14 +22,24 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.let {
-            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
-            // Menampilkan di tempat yang sesuai
-            textTurn.text = "$playerName's Turn"
+//            val playerName = GameFragmentArgs.fromBundle(requireArguments()).skorAkhir
+//            // --Menampilkan di tempat yang sesuai
+//            textTurn.text = "$playerName's Turn"
+
+            val skorTerakir  = GameFragmentArgs.fromBundle(requireArguments()).skorAkhir
+            // --Menampilkan di tempat yang sesuai
+            playerScore.text = "Your score is $skorTerakir"
         }
         buttonBack.setOnClickListener {
-            val action= GameFragmentDirections.actionMainFragment()
+//            val action= GameFragmentDirections.actionMainFragment()
+//            Navigation.findNavController(it).navigate(action)
+            val action= GameFragmentDirections.actionGameFragmentToMainFragment()
             Navigation.findNavController(it).navigate(action)
+
+
         }
+
+
     }
 
 
--- app/src/main/java/id/ac/ubaya/adv160419107week2/MainFragment.kt
+++ app/src/main/java/id/ac/ubaya/adv160419107week2/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_main.*
+import kotlin.random.Random
 
 
 class MainFragment : Fragment() {
@@ -25,10 +26,47 @@ class MainFragment : Fragment() {
             // Ambil nama pemain
             val playerName = editName.text.toString()
             // Dapatkan action yang ingin dijalankan
-            val action = MainFragmentDirections.actionGameFragment(playerName)
-            // Jalankan action tersebut
-            Navigation.findNavController(it).navigate(action)
+//            val action = MainFragmentDirections.actionGameFragment(playerName)
+//            // Jalankan action tersebut
+//            Navigation.findNavController(it).navigate(action)
         }
+
+
+//        --Random
+        val randomNumber1  = Random.nextInt(0,50)
+        val randomNumber2 = Random.nextInt(0,30)
+//      -- Ditampilkan
+        num1.text= randomNumber1.toString()
+        num2.text = randomNumber2.toString()
+
+        var skor =0
+        buttonSubmit.setOnClickListener {
+
+            val total :Int= num1.text.toString().toInt() +num2.text.toString().toInt()
+
+            val jawab =inputAnswer.text.toString().toInt()
+
+            if(jawab == total){
+
+                skor += 1
+                textScore.text= skor.toString()
+                val randomNumber1  = Random.nextInt(0,50)
+                val randomNumber2 = Random.nextInt(0,30)
+                num1.text= randomNumber1.toString()
+                num2.text = randomNumber2.toString()
+                inputAnswer.setText("")
+            }
+            else{
+
+                // Dapatkan action yang ingin dijalankan
+            val action = MainFragmentDirections.actionMainFragmentToGameFragment(skor)
+//            // Jalankan action tersebut
+            Navigation.findNavController(it).navigate(action)
+            }
+
+
+        }
+
     }
 
 
